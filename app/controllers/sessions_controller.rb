24b218d78c57083bcf2efe32349598b28fc6dbb9
George Melnikov <skywalqqer@yandex.ru>
2016-04-03 16:35:08 +0400

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
 		flash[:success] = 'Good bye!'
 		redirect_to root_url
 	end
-
+	
 	def auth_fail
 		render text: "You've tried to authenticate via #{params[:strategy]}, but the following error occurred: #{params[:message]}", status: 500
 	end
