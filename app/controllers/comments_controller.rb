ff351838fc17e3c0e78327289049e0536b4e699e
George Melnikov <skywalqqer@yandex.ru>
2016-04-06 19:55:57 +0400

    update code style

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,21 @@
 class CommentsController < ApplicationController
-	def new
-		@comment = Comment.new
-		@comments = Comment.order('created_at DESC')
-	end
+  def new
+    @comment = Comment.new
+    @comments = Comment.order('created_at DESC')
+  end
 
-	def create
-		@comment = current_user.comments.build(comment_params)
-		if @comment.save
-			flash[:success] = 'Your comment was posted. Thank you!'
-		else
-			flash[:error] = 'Your comment cannot be post'
-		end
-		redirect_to root_url
-	end
-
-	private
-	def comment_params
-		params.require(:comment).permit(:body)
-	end
+  def create
+    @comment = current_user.comments.build(comment_params)
+    if @comment.save
+      flash[:success] = 'Your comment was posted. Thank you!'
+    else
+      flash[:error] = 'Your comment cannot be post'
+    end
+    redirect_to root_url
+  end
+  
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
